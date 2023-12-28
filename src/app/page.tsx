6076b25c1d12672c70dd90edff2afb873a456594
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-29 01:17:20 +0200

    SignIn/SignOut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
   const session = await auth();
   return (
     <div className="flex">
-      {/* <form action={actions.signIn}>
+      <form action={actions.signIn}>
         <Button type="submit">signIn</Button>
       </form>
       <form action={actions.signOut}>
         <Button type="submit">signOut</Button>
-      </form> */}
+      </form>
       {/* {session?.user ? <h1>{JSON.stringify(session.user)}</h1> : <h1>Sign In</h1>} */}
       {/* <Profile/> */}
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,6 +15,26 @@ import Link from "next/link";
 async function Header() {
   const session = await auth();
 
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = <Avatar src={session.user.image || ""} />;
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <Button type="submit" color="secondary" variant="bordered">
+            Sign In
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button type="submit" color="secondary" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </>
+    );
+  }
+
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -28,9 +48,7 @@ async function Header() {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem>
-            {session?.user ? <div>Signed In</div>: <div>Signed Out</div>}
-        </NavbarItem>
+        {authContent}
       </NavbarContent>
     </Navbar>
   );
